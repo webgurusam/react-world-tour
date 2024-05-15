e079b4802cd7fb275ee889bb280bb6e35dc8bcfd
webgurusam <shyamghosh666@gmail.com>
2024-05-15 08:30:05 +0600

    project initialization

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,13 @@
+import './Country.css'
+
+const Country = ({country}) => {
+    const {name, flags} = country;
+    return (
+        <div className='country'>
+            <h3>Name : {name?.common}</h3>
+            <img src={flags?.png} alt={flags?.alt} />
+        </div>
+    );
+};
+
+export default Country;
